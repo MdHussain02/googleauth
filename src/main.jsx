6d7b6f422feeed60855bd8935cb7a0f1283b8062
muhammedhussainn <258hussain258@gmail.com>
2025-02-05 20:24:34 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import App from './App.jsx'
+import App from './App.jsx';
 import { RecoilRoot } from 'recoil';
 
 createRoot(document.getElementById('root')).render(
-    <RecoilRoot>
-    <GoogleOAuthProvider clientId="76936609704-q6s2ba027jccad1s80juji07vneh893o.apps.googleusercontent.com">
-    <App />
+  <RecoilRoot>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
     </GoogleOAuthProvider>
-    </RecoilRoot>
-
-
-)
+  </RecoilRoot>
+);
