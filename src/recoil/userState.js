cd08379ebb80e632786d82392ba8603573304cde
muhammedhussainn <258hussain258@gmail.com>
2025-02-05 21:44:13 +0530

    Update userState.js

--- src/recoil/userState.js
+++ src/recoil/userState.js
@@ -1,7 +1,6 @@
 // src/recoil/userState.js
 import { atom } from 'recoil';
-
 export const userState = atom({
-  key: 'userState', // unique ID (with respect to other atoms/selectors)
-  default: JSON.parse(localStorage.getItem('user')) || null, // Persist user state in localStorage
+  key: 'userState', 
+  default: JSON.parse(localStorage.getItem('user')) || null, 
 });
