d8044e04c1db36d8d1a499a51b42bbf614ca59d1
muhammedhussainn <258hussain258@gmail.com>
2025-02-06 22:18:52 +0530

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -12,12 +12,11 @@ const selectors = [
 
 const SideBar = () => {
   const isMobile = typeof window !== "undefined" && window.innerWidth < 1024;
-
   return (
     <>
       {/* Desktop Sidebar */}
       {!isMobile && (
-        <div className="w-16 lg:w-64 bg-amber-900 text-slate-300 py-6 px-2 rounded-2xl shadow-xl transition-all duration-300 m-4 min-h-[90vh] flex flex-col">
+        <div className="w-16 lg:w-50 bg-amber-900 text-slate-300 py-6 px-2 rounded-2xl shadow-xl transition-all duration-300 m-2 min-h-[90vh] flex flex-col">
           <nav className="flex-1 px-2">
             {selectors.map(({ id, icon: Icon, link, exact }) => (
               <NavLink
@@ -25,7 +24,7 @@ const SideBar = () => {
                 to={link}
                 end={exact}
                 className={({ isActive }) =>
-                  `flex items-center px-3 py-2.5 mb-1 rounded-lg transition-all duration-200 ease-in-out
+                  `flex items-center py-2.5 mb-1 rounded-lg transition-all duration-200 ease-in-out
                   ${isActive ? "bg-amber-200 text-black shadow-md" : "text-slate-400 hover:bg-amber-400 hover:text-white"}
                   lg:justify-start justify-center`
                 }
